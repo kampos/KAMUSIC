cb295a890ba1406728740e48c4bd0f4126afd8e9
kampos <kampos.info@gmail.com>
2026-05-30 21:52:00 +0200

    run app non-unique in snap

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,15 @@
 use adw::prelude::*;
+use gtk::gio;
 
 use crate::ui::window::MainWindow;
 
 pub const APP_ID: &str = "org.kampos.kamusic";
 
 pub fn run() {
-    let app = adw::Application::builder().application_id(APP_ID).build();
+    let app = adw::Application::builder()
+        .application_id(APP_ID)
+        .flags(gio::ApplicationFlags::NON_UNIQUE)
+        .build();
 
     app.connect_activate(|app| {
         let window = MainWindow::new(app);
